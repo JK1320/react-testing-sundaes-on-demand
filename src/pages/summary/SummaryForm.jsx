f05592cdfa1e760826b4f95a3ddd044131de570f
JK1320 <kaurjus1@gmail.com>
2022-05-14 22:48:59 +0100

    added form to test

--- src/pages/summary/SummaryForm.jsx
+++ src/pages/summary/SummaryForm.jsx
@@ -1,8 +1,30 @@
+import React, {useState} from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+
+
 const SummaryForm = () => {
+    const [toChecked, setToChecked] = useState(false);
+
+    const checkboxLabel = (
+        <span>
+            I agree to <span style={{color: 'blue'}}>Terms and conditions</span>
+        </span>
+    );
+
     return (
-        <div>
-            
-        </div>
+        <Form>
+            <Form.Group controlId="terms-and-conditions">
+                <Form.check 
+                type="checkbox"
+                checked={toChecked}
+                onChange={(e) => setToChecked(e.target.checked)}
+                label={checkboxLabel}
+                />
+            </Form.Group>
+            <Button variant="primary" type="submit" disabled={!toChecked}>Confirm Order</Button>
+        </Form>
     )
 }
 
