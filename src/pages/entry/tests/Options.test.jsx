b0f50b25df6582bc379dfb783fc6b84475373f9e
JK1320 <kaurjus1@gmail.com>
2022-05-20 23:33:08 +0100

    test to see display of images for scoops

---
+++ src/pages/entry/tests/Options.test.jsx
@@ -0,0 +1,17 @@
+import { render, screen } from "@testing-library/react"
+import Options from '../Options';
+
+
+test('displays image for each scoop option from server', () => {
+    render(<Options optionType="scoops" />);
+
+    // find images  $ => means at the end of string
+    const scoopImages = screen.getAllByRole('img', {name: '/scoop$/i'});
+    expect(scoopImages).toHaveLength(2);
+    
+    // confirm alt text of images
+    const altText = scoopImages.map(element => element.alt);
+
+    // altText will have array of alt text, array/object use toEqual() method
+    expect(altText).toEqual(['Chocolate scoop', 'Vanilla scoop']);
+})
