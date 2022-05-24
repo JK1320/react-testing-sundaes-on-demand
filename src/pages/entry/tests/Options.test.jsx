f31dc75f8bd7129823e33144b126305fbe8eccde
JK1320 <kaurjus1@gmail.com>
2022-05-25 00:31:48 +0100

    added test to check display of toppings' images

--- src/pages/entry/tests/Options.test.jsx
+++ src/pages/entry/tests/Options.test.jsx
@@ -16,3 +16,20 @@ test('displays image for each scoop option from server', async () => {
     // altText will have array of alt text, array/object use toEqual() method
     expect(altText).toEqual(['Chocolate scoop', 'Vanilla scoop']);
 })
+
+test('Displays image for each toppings option from server', async () => {
+// Mock Service Worker will return 3 toppings from server
+render(<Options optionType="toppings" />);
+
+// find images, expect 3 based on what msw returns
+const images = await screen.findAllByRole('img', {name: /topping$/i});
+expect(images).toHaveLength(3);
+
+// check the actual alt text for the images
+const imageTitles = images.map((img) => img.alt);
+expect(imageTitles).toEqual([
+    'Cherries topping',
+    'M&Ms topping',
+    'Hot fudge topping',
+]);
+});
