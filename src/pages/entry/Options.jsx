f59ccd693d795c22f5b41fa42a56a3eaea670b43
JK1320 <kaurjus1@gmail.com>
2022-06-09 00:08:18 +0100

    import pricePerItem & useOrderDetails

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -4,30 +4,47 @@ import Row from "react-bootstrap/Row";
 import AlertBanner from "../common/AlertBanner";
 import ScoopOption from './ScoopOption';
 import ToppingOption from "./ToppingOption";
-
+import {pricePerItem} from "../../constants";
+import {useOrderDetails} from "../../contexts/OrderDetails";
 
 
 export default function Options({ optionType }) {
 const [items, setItems] = useState([]);
 const [error, setError] = useState(false);
+const [orderDetails, updateItemCount] = useOrderDetails();
 
-
-  // optionType is either 'scoops' or 'toppings'
-  useEffect(() => {
-    axios.get(`http://localhost:3030/${optionType}`)
-    .then(response => setItems(response.data))
-    .catch(error => {
-        setError(true);
+// optionType is either 'scoops' or 'toppings'
+useEffect(() => {
+  axios
+    .get(`http://localhost:3030/${optionType}`)
+    .then((response) => setItems(response.data))
+    .catch((error) => {
+      setError(true);
     });
-  }, [optionType]);
+}, [optionType]);
 
-  if(error){
-    return <AlertBanner />
-  }
+if (error) {
+  return <AlertBanner />;
+}
 
-  const ItemComponent = optionType === 'scoops' ? ScoopOption : ToppingOption;
+const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption;
+const title = optionType[0].toUpperCase() + optionType.slice(1).toLowerCase();
 
-  const optionItems = items.map((item) => <ItemComponent key={item.name} name={item.name} imagePath={item.imagePath} />)
+const optionItems = items.map((item) => (
+  <ItemComponent 
+  key={item.name} 
+  name={item.name} 
+  imagePath={item.imagePath} 
+  updateItemCount={(itemName, newItemCount) => updateItemCount(itemName, newItemCount, optionType)}
+  />
+));
 
-  return <Row>{optionItems}</Row>;
+return (
+  <>
+    <h2>{title}</h2>
+    <p>{pricePerItem[optionType]} each</p>
+    <p>{title} total: {orderDetails.totals[optionType]}</p>
+    <Row>{optionItems}</Row>
+  </>
+); 
 }
