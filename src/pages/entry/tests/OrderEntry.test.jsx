42b104c08f07e8668d8bb6d6a5ac0b0c926ef0de
JK1320 <kaurjus1@gmail.com>
2022-07-16 22:54:29 +0100

    paases a prop to orderEntry component

--- src/pages/entry/tests/OrderEntry.test.jsx
+++ src/pages/entry/tests/OrderEntry.test.jsx
@@ -14,7 +14,8 @@ server.resetHandlers(
     )
 )
 
-render(<OrderEntry />);
+render(<OrderEntry setOrderPhase={jest.fn()} />);
+
 await waitFor(async()=> {
 const alerts = await screen.findAllByRole('alert');
 
