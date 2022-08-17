cd0600fc08ad2ca3ff348c490d7c6d0815436f42
JK1320 <kaurjus1@gmail.com>
2022-08-17 17:37:18 +0100

    added disabled option for order button

--- src/pages/entry/OrderEntry.jsx
+++ src/pages/entry/OrderEntry.jsx
@@ -4,13 +4,17 @@ import {useOrderDetails} from "../../contexts/OrderDetails";
 
 export default function OrderEntry({setOrderPhase}) {
   const [orderDetails] = useOrderDetails();
+
+  // disable order button if there aren't any scoops ordered
+const orderDisabled = orderDetails.totals.scoops === "$0.00";
+
     return (
       <div>
         <h1>Design Your Sundae!</h1>
         <Options optionType="scoops" />
         <Options optionType="toppings" />
         <h2>Grand Total: {orderDetails.totals.grandTotal}</h2>
-        <Button onClick={() => setOrderPhase("review")}>Order Sundae!</Button>
+        <Button disabled={orderDisabled} onClick={() => setOrderPhase("review")}>Order Sundae!</Button>
       </div>
     ); 
 };
