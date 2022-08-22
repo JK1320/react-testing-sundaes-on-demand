7e41bab37970fea53f41e0f4984f2aafb60b12a6
JK1320 <kaurjus1@gmail.com>
2022-08-22 17:25:16 +0100

    added useState to check valid scoop count

--- src/pages/entry/ScoopOption.jsx
+++ src/pages/entry/ScoopOption.jsx
@@ -1,13 +1,27 @@
+import {useState} from "react";
 import Col from 'react-bootstrap/Col';
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 
 
 export default function ScoopOption({ name, imagePath, updateItemCount }) {
-const handleChange = (event) => {
-  updateItemCount(name, event.target.value);
-}
+  const [isValid, setIsValid] = useState(true);
 
+const handleChange = (event) => {
+  const currentValue = event.target.value;
+  updateItemCount(name, currentValue);
+
+
+// make sure we are using a number and not a string to validate
+const currentValueFloat = parseFloat(currentValue);
+
+// validate
+setIsValid(
+  0 <= currentValueFloat &&
+    currentValueFloat <= 10 &&
+    Math.floor(currentValueFloat) === currentValueFloat
+);
+};
   return (
 <Col xs={12} sm={6} md={4} lg={3} style={{textAlign: 'center'}}>
       <img style={{width: '75%'}} src={`http://localhost:3030/${imagePath}`} alt={`${name} scoop`} />
@@ -17,10 +31,10 @@ const handleChange = (event) => {
         <Form.Control 
         type="number" 
         defaultValue={0}
-        onChange={handleChange} />
+        onChange={handleChange}
+        isValid={!isValid} />
         </Col>
       </Form.Group>
   </Col>
-  );
-  
+  ); 
 }
