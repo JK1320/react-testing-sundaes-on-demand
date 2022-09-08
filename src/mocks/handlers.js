56b4712c426daea8d11150427c825fd5cbef4b9a
JK1320 <kaurjus1@gmail.com>
2022-09-08 13:57:01 +0100

    error correction

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -23,6 +23,6 @@ export const handlers = [
   }),
 
   rest.post("http://localhost:3030/order", (req, res, ctx) => {
-    return res(ctx.json({ordrNumber: 123455676}))
+    return res(ctx.json({orderNumber: 123455676}))
   })
 ];
