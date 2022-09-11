6d3bdcfb59463199bbbf8ff8c166668c2240c47a
JK1320 <kaurjus1@gmail.com>
2022-09-11 23:58:49 +0100

    format code

--- src/test-utils/testing-library-utils.jsx
+++ src/test-utils/testing-library-utils.jsx
@@ -1,7 +1,7 @@
 import {render} from "@testing-library/react";
 import {OrderDetailsProvider} from "../contexts/OrderDetails";
 
-const renderWithContext = (ui, options) => render(ui, {wrapper: OrderDetailsProvider, ...options})
+const renderWithContext = (ui, options) => render(ui, {wrapper: OrderDetailsProvider, ...options});
 
 // re-export everything
 
